728185260c8ec8ae3f4de97985ba3a90985a7f5d
Ubuntu <ubuntu@ip-172-31-47-173.ap-northeast-2.compute.internal>
2022-06-08 20:45:21 +0000

    ADD: environment variables

--- src/configs/auth.config.js
+++ src/configs/auth.config.js
@@ -1,3 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+
+const envFound = dotenv.config({ path: path.resolve("../.env") });
+if (envFound.error) {
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
 export default {
-  secret: "planby-secret-key",
+  secret: process.env.ACCESS_TOKEN_SECRET,
 };
