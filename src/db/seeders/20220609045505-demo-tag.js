8a97fcd686a831944a6f96491bf325031c909146
Ubuntu <ubuntu@ip-172-31-47-173.ap-northeast-2.compute.internal>
2022-06-10 16:43:25 +0000

    ADD: seed tag values

--- src/db/seeders/20220609045505-demo-tag.js
+++ src/db/seeders/20220609045505-demo-tag.js
@@ -6,10 +6,26 @@ module.exports = {
       "Tags",
       [
         {
+          uid: "042E9B32697380",
+          name: "카이스트",
+          latitude: 127.36128608273715,
+          longitude: 36.37049682178313,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
           uid: "042E9F32697380",
           name: "이화여자대학교",
           latitude: 126.94765009467245,
           longitude: 37.562544705628845,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          uid: "04F21BFA9D7180",
+          name: "역삼역 4번 출구",
+          latitude: 127.03515980477283,
+          longitude: 37.500516238601826,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
